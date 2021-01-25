4a3654094de812f00753b6806a29595afe876d6a
wellington <wellingtonslpbi@hotmail.com>
2021-01-25 20:23:59 -0300

    add styled-components and reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import GlobalStyle from './styles/globals';
+
 const App: React.FC = () => {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <h1>Hello World</h1>
-    </div>
+    </>
   );
 };
 
